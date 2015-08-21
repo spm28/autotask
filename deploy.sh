3b2ea8ffed67f6aa1bb356b3ff8cdcf6c6689d6c
unknown <Siarhei_Pahin@EPBYMINW2473.minsk.epam.com>
2015-08-21 14:01:18 +0300

    Files

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+mkdir -p /usr/share/jboss/jboss-as/server/default/deploy/ROOT.war/autodeploy
+cd /usr/share/jboss/jboss-as/server/default/deploy/ROOT.war/autodeploy
+mkdir -p dist
+mkdir -p current
+mkdir -p old
+rm -rf dist/*
+rm -rf old/*
+cp -rf current/* old/
+rm -rf current/*
+cp -rf ../../../tmp/Autodeploy.tar.gz current/
+tar -zxvf current/Autodeploy.tar.gz
+mv index.html dist/
